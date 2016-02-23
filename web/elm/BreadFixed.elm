cc942a522e375174cb2415a60e811db704dd03c0
Karl Eklund <localpart@gmail.com>
2016-02-23 21:30:44 +0100

    Indentation

--- web/elm/BreadFixed.elm
+++ web/elm/BreadFixed.elm
@@ -1,28 +1,43 @@
-module BreadFixed where
+module BreadFixed (..) where
 
 import StartApp.Simple
 import Html exposing (text, Html, p)
 import Html.Events exposing (onClick)
 
+
 main : Signal Html
 main =
   StartApp.Simple.start
-          { model = init
-          , update = update
-          , view = view
-          }
+    { model = init
+    , update = update
+    , view = view
+    }
 
-type alias IsFixed = Bool
-type alias Model = IsFixed
+
+type alias IsFixed =
+  Bool
+
+
+type alias Model =
+  IsFixed
+
 
 init : Model
-init = False
+init =
+  False
+
 
 toYesNo : IsFixed -> String
 toYesNo isfixed =
-  if isfixed then "Yes" else "No"
+  if isfixed then
+    "Yes"
+  else
+    "No"
 
-type Action = Toggle Model
+
+type Action
+  = Toggle Model
+
 
 view : Signal.Address Action -> Model -> Html
 view address model =
@@ -30,8 +45,12 @@ view address model =
     [ onClick address (Toggle model) ]
     [ text (toYesNo model) ]
 
+
 update : Action -> Model -> Model
 update action model =
   case model of
-    True -> False
-    False -> True
+    True ->
+      False
+
+    False ->
+      True
