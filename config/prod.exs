8b06e90dc631da3b36925ee34f7a2fcd29aad822
Karl Eklund <localpart@gmail.com>
2016-03-23 15:24:10 +0100

    fix host in production

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bread_fixed, BreadFixed.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "bread-fixed.karleklund.se", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
